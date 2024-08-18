9a6a086bf1212f271ae4ad03ba8083ad30c17041
Nathan Schrader <natecschrader@gmail.com>
2024-08-17 18:14:01 -0700

    connect Spotti entity to the root connection

--- spotti/src/app.module.ts
+++ spotti/src/app.module.ts
@@ -3,13 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SpottisModule } from './spottis/spottis.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Spotti } from './spottis/spotti.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [],
+      entities: [Spotti],
       synchronize: true,
     }),
     SpottisModule,
