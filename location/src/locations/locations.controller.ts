c955df5fe2b2732d33d4a7affeece8bea544b880
Nathan Schrader <natecschrader@gmail.com>
2024-08-27 15:37:16 -0700

    Add new Locations service to the monorepo

---
+++ location/src/locations/locations.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('locations')
+export class LocationsController {
+  @Post()
+  createLocation() {
+
+  }
+
+  @Get()
+  getLocation() {
+
+  }
+
+  @Get()
+  getLocations() {
+
+  }
+
+}
