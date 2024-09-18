0827b1a63ce5463253273c3db834d6e6433f09ca
Nathan Schrader <natecschrader@gmail.com>
2024-09-18 09:29:48 -0700

    Updates

--- common/src/dtos/create-user.dto.ts
+++ common/src/dtos/create-user.dto.ts
@@ -1,10 +1,14 @@
 import { IsArray, IsEmail, IsNumber, IsOptional, IsString } from 'class-validator';
 import { Spotti } from '../entities/spotti.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+
   @IsEmail()
+  @ApiProperty({ example: 'user@example.com', description: 'User email' })
   email!: string;
 
   @IsString()
+  @ApiProperty({ example: 'password123', description: 'User password' })
   password!: string;
 }
