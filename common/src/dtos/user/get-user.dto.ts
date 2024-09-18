1a5d7728c4296434e4d731652b628f4c6be18c1c
Nathan Schrader <natecschrader@gmail.com>
2024-09-18 12:21:02 -0700

    Updates

--- common/src/dtos/user/get-user.dto.ts
+++ common/src/dtos/user/get-user.dto.ts
@@ -1,8 +1,9 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNumber } from "class-validator";
+import { IsNotEmpty, IsNumber } from "class-validator";
 
 export class GetUserRequestDto {
   @IsNumber()
+  @IsNotEmpty()
   @ApiProperty({ example: 1, description: 'The ID of the user' })
   id!: number;
 }
