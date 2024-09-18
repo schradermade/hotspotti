bf73cae68ae4034d4ffa70c0c099581349eea51a
Nathan Schrader <natecschrader@gmail.com>
2024-09-18 12:54:27 -0700

    wire up global ValidationPipe

--- auth/src/main.ts
+++ auth/src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as morgan from 'morgan';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   // Swagger config
   const config = new DocumentBuilder()
     .setTitle('HotSpotti - Auth service')
@@ -17,7 +19,18 @@ async function bootstrap() {
   // Swagger Ui
   SwaggerModule.setup('api-docs', app, document);
 
+  // morgan logging
   app.use(morgan('combined'));
+
+  // setup ValidationPipe globally
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(3000, () => {
     console.log('AuthService: Listening on PORT 3000!!!');
   });
