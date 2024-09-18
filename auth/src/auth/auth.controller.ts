c3c4d89d6c80eb41480222087b3ab3fcbdf297ba
Nathan Schrader <natecschrader@gmail.com>
2024-09-18 10:11:36 -0700

    add swagger decorators to endpoint handlers

--- auth/src/auth/auth.controller.ts
+++ auth/src/auth/auth.controller.ts
@@ -8,17 +8,24 @@ import {
 } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreateUserDto } from '@hotspotti/common';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly httpService: HttpService) {}
 
   @Post('/signup')
-  async createUser(@Body() body: any): Promise<any> {
+  @ApiOperation({ summary: 'Create user profile' })
+  @ApiResponse({ status: 200, description: 'Profile successfully created' })
+  @ApiResponse({ status: 500, description: 'User creation failed' })
+  async createUser(@Body() body: CreateUserDto): Promise<any> {
     const payload = { email: body.email, password: body.password };
 
     try {
-      const userServiceUrl = 'http://user-srv:3000/users/signup';
+      const userServiceUrl =
+        'http://user-srv-internal.default.svc.cluster.local:3000/users/signup';
       const response = await firstValueFrom(
         this.httpService.post(userServiceUrl, payload),
       );
@@ -32,16 +39,12 @@ export class AuthController {
     }
   }
 
-  @Get('/get-user')
-  async getUserData(): Promise<any> {
-    const userServiceUrl = 'http://user-srv:3000/users/hello';
+  @Get('/profile')
+  async getUserData(@Body() body: any): Promise<any> {
+    const { id } = body;
+    const userServiceUrl = `http://user-srv-internal.default.svc.cluster.local:3000/users/${id}`;
     const response = await firstValueFrom(this.httpService.get(userServiceUrl));
 
     return response.data;
-  }
-
-  @Get('/test')
-  testRoute() {
-    return 'Test route from auth service';
   }
 }
