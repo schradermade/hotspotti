7f4cffc1b68df10d4a3d0d5bcd6ae4e6c2e22a5e
Nathan Schrader <natecschrader@gmail.com>
2024-09-18 17:15:57 -0700

    install npm pack swagger/ui; wire up

--- user/src/main.ts
+++ user/src/main.ts
@@ -2,9 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as morgan from 'morgan';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Hotspotti - User service')
+    .setDescription('API documentation for the User service')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Swagger UI
+  SwaggerModule.setup('api-docs', app, document);
+
   app.use(morgan('combined'));
   app.useGlobalPipes(new ValidationPipe());
 
