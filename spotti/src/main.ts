0d94765e82f80d9b662a6d421b6708b310d34fc4
Nathan Schrader <natecschrader@gmail.com>
2024-09-18 17:08:36 -0700

    install swagger/ui-express npm; wire up

--- spotti/src/main.ts
+++ spotti/src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as morgan from 'morgan';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger config
+  const config = new DocumentBuilder()
+    .setTitle('HotSpotti - Spotti service')
+    .setDescription('API documentation for Spotti service')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Swagger UI
+  SwaggerModule.setup('api-docs', app, document);
+
+  // morgan logging
   app.use(morgan('combined'));
   await app.listen(3000);
 }
