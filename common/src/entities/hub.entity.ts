46b46bfb45c8bdf2d880be2b23c89e6a6542c2c8
Nathan Schrader <natecschrader@gmail.com>
2024-09-19 09:42:31 -0700

    Updates

--- common/src/entities/hub.entity.ts
+++ common/src/entities/hub.entity.ts
@@ -1,19 +1,31 @@
 import { Entity, Column, PrimaryGeneratedColumn, ManyToMany, JoinTable,  } from "typeorm";
 import { Spotti } from "./spotti.entity";
 import { User } from "./user.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity()
 export class Hub {
   @PrimaryGeneratedColumn()
+  @ApiProperty({ example: 1, description: 'The unique identifier of the Hub' })
   id!: number;
 
   @Column()
+  @ApiProperty({ example: 'NW 23rd Avenue', description: 'The name of the Hub' })
   name!: string;
 
   @Column({ type: 'text' })
+  @ApiProperty({
+    example: '45.5231,-122.6765',
+    description: 'Geospatial boundaries for highlighting the Hub area on the map',
+  })
   boundaries!: string; // Geospatial boundaries for highlighting the Hub area on the map
 
   @Column({ type: 'text', nullable: true })
+  @ApiProperty({
+    example: 'A popular shopping district with restaurants and cafes',
+    description: 'Optional description of the Hub',
+    required: false,
+  })
   description?: string; // Optional description of the Hub
 
   // added after issue fixed
