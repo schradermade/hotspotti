41b6bfe888b7f77bce54a3a41965c8f2e2540e05
Nathan Schrader <natecschrader@gmail.com>
2024-09-30 10:24:46 -0700

    Updates

---
+++ common/src/constants/category.ts
@@ -0,0 +1,16 @@
+export enum Category {
+  Landmarks = 'Landmarks',
+  Historical = 'Historical',
+  Parks = 'Parks',
+  FamousStreets = 'Famous Streets',
+  Museums = 'Museums',
+  NaturalWonders = 'Natural Wonders',
+  Beaches = 'Beaches',
+  Entertainment = 'Entertainment',
+  Shopping = 'Shopping',
+  Dining = 'Dining',
+  ScenicViews = 'Scenic Views',
+  Religious = 'Religious Sites',
+  Monuments = 'Monuments',
+  NightLife = 'Night Life'
+}
--- common/src/dtos/spotti/create-spotti.dto.ts
+++ common/src/dtos/spotti/create-spotti.dto.ts
@@ -3,22 +3,28 @@
 
 import { IsArray, IsEnum, IsNumber, IsObject, IsOptional, IsString } from 'class-validator';
 import { BestTimeToVisit } from '../../constants/bestTimeToVisit';
+import { Category } from '../../constants/category';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateSpottiDto {
   @IsOptional()
   @IsString()
+  @ApiProperty({ example: 'Statue of Liberty', description: 'Name of the Spotti' })
   name!: string;
 
   @IsOptional()
   @IsString()
+  @ApiProperty({ example: 'Statue of Liberty', description: 'The Statue of Liberty is an iconic symbol of freedom located on Liberty Island in New York Harbor. Gifted to the United States by France in 1886, the statue depicts a robed woman representing Libertas, the Roman goddess of liberty, holding a torch in one hand and a tablet inscribed with the date of the American Declaration of Independence in the other. Standing 305 feet tall, the statue has become a symbol of hope and democracy, welcoming millions of immigrants arriving by sea to the U.S.' })
   description!: string;
 
   @IsOptional()
   @IsString()
+  @ApiProperty({ example: '155', description: '' })
   locationId!: string;
 
   @IsOptional()
-  @IsString()
+  @IsEnum(Category, {message: 'category must be one of the following: Landmarks, Historical, Parks, Famous Streets, Museums, Natural Wonders, Beaches, Entertainment, Shopping, Dining, Scenic Views, Religious Sites, Monuments, Night Life.'} )
+  @ApiProperty({ example: 'Landmarks', description: 'Category name of the Spotti.' })
   category!: string;
 
   @IsOptional()
