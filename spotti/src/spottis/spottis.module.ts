b2a8d59785ca0165a4727f672b00c77cd525f549
Nathan Schrader <natecschrader@gmail.com>
2024-10-09 14:20:20 -0700

    import and wire up SqsModule, SchedulerModule for SQS

--- spotti/src/spottis/spottis.module.ts
+++ spotti/src/spottis/spottis.module.ts
@@ -1,13 +1,24 @@
 import { Module } from '@nestjs/common';
-import { SpottisController } from './spottis.controller';
 import { SpottisService } from './spottis.service';
+import { SpottisController } from './spottis.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Hub, Spotti } from '@hotspotti/common';
+import { Hub, Spotti, SqsModule } from '@hotspotti/common';
+import { SpottiMessagePollingService } from './spotti-message-polling.service';
+import { SpottiMessageProcessorService } from './spotti-message-processor.service';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Spotti, Hub])],
+  imports: [
+    TypeOrmModule.forFeature([Spotti, Hub]),
+    SqsModule,
+    ScheduleModule.forRoot(),
+  ],
   controllers: [SpottisController],
   exports: [SpottisService],
-  providers: [SpottisService],
+  providers: [
+    SpottisService,
+    SpottiMessagePollingService,
+    SpottiMessageProcessorService,
+  ],
 })
 export class SpottisModule {}
