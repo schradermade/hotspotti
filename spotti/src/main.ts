0eff592a7437ced73afa2140ba79c7380385a5cd
Nathan Schrader <natecschrader@gmail.com>
2024-10-09 14:19:09 -0700

    import and wire up dotenv

--- spotti/src/main.ts
+++ spotti/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 import * as morgan from 'morgan';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
