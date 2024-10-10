0215802a2d59fa1a6b8c8121995e9f056e15a7e5
Nathan Schrader <natecschrader@gmail.com>
2024-10-09 17:21:29 -0700

    Updates

--- common/src/services/app-config.service.ts
+++ common/src/services/app-config.service.ts
@@ -1,16 +1,27 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppConfigService {
-  constructor(private readonly configService: ConfigService) {} // Inject it here
+  constructor(private readonly configService: ConfigService) {}
 
   getServiceBaseUrl(serviceEnvKey: string): string {
-    const url = this.configService.get<string>(serviceEnvKey);
-    if (!url) {
-      throw new Error(`${serviceEnvKey} is not defined in the env variables`);
+    const serviceBaseUrl = this.configService.get<string>(serviceEnvKey);
+    
+    if (!serviceBaseUrl) {
+      throw new Error(`Service base URL for ${serviceEnvKey} is not defined in environment variables`);
     }
-    return url;
+    
+    return serviceBaseUrl;
   }
 
+  getJwtSecret(): string {
+    const jwtSecret = this.configService.get<string>('JWT_SIGN_SECRET');
+    if (!jwtSecret) {
+      throw new Error('JWT_SECRET is not defined in environment variables');
+    }
+    
+    return jwtSecret;
+  }
 }
+
