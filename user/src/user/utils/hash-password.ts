6d7b069fbc5b27a5ce61548bb7a027f0b77de3dd
Nathan Schrader <natecschrader@gmail.com>
2024-11-01 10:50:43 -0700

    refactor hashing helpers

--- user/src/user/utils/hash-password.ts
+++ user/src/user/utils/hash-password.ts
@@ -3,10 +3,22 @@ import { promisify } from 'util';
 
 const scrypt = promisify(_scrypt);
 
-export async function hashPassword(password: string) {
-  const salt = randomBytes(8).toString('hex');
+export async function hashPassword(password: string): Promise<{
+  hashedPassword: string;
+  salt: string;
+}> {
+  const salt = randomBytes(16).toString('hex');
   const hash = (await scrypt(password, salt, 32)) as Buffer;
-  const hashedPass = salt + '.' + hash.toString('hex');
+  const hashedPassword = salt + '.' + hash.toString('hex');
 
-  return hashedPass;
+  return { hashedPassword, salt };
+}
+
+export async function validatePassword(
+  password: string,
+  storedHash: string,
+): Promise<boolean> {
+  const [salt, originalHash] = storedHash.split('.');
+  const hash = (await scrypt(password, salt, 32)) as Buffer; // Hash the provided password using the same salt
+  return hash.toString('hex') === originalHash; // Compare the hash of the provided password with the stored hash
 }
