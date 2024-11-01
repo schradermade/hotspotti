7db04d8dc0984bb29144ee56f71aff79573cba7d
Nathan Schrader <natecschrader@gmail.com>
2024-11-01 10:44:32 -0700

    wire up enableCors

--- auth/src/main.ts
+++ auth/src/main.ts
@@ -7,6 +7,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   // Swagger config
   const config = new DocumentBuilder()
     .setTitle('HotSpotti - Auth service')
